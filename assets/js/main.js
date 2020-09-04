543a3397151e9dbe80b628951d991da868034e28
Noah Williams <public@noahsbwilliams.com>
2020-09-04 13:07:07 -0800

    Change Ghost Admin URL

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,7 +9,7 @@ jQuery(document).ready(function($) {
         'infinite-scroll': false,
         'infinite-scroll-step': 999,
         'disqus-shortname': 'noahsbwilliams',
-        'content-api-host': 'https://noahsbwilliams.com',
+        'content-api-host': 'https://ghost.noahsbwilliams.com',
         'content-api-key': 'a226adc6ba2cd7639734e38863',
     };
 
